7fbdf3df8ec1ae52ea781b7e3a30c3b51f008134
RecklessRyujin <recklesscollective1920@gmail.com>
2026-07-06 02:05:32 -0400

    Update theme application method in MainActivity

--- app/src/main/java/com/gamescout/MainActivity.java
+++ app/src/main/java/com/gamescout/MainActivity.java
@@ -18,8 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ThemeManager.applyTheme(this);
-        //ThemeManager.init(this);
+        ThemeManager.applySavedTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
