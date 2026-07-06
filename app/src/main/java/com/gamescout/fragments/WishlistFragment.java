0233c321c2183b3f7e91b9acf5bb9e54d21de86f
RecklessRyujin <recklesscollective1920@gmail.com>
2026-07-06 00:00:57 -0400

    Add WishlistFragment class for wishlist feature

---
+++ app/src/main/java/com/gamescout/fragments/WishlistFragment.java
@@ -0,0 +1,15 @@
+package com.gamescout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class WishlistFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(android.R.layout.simple_list_item_1, container, false);
+    }
+}
