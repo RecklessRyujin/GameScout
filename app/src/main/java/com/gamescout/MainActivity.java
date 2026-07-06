526f2f0bb3eab9ac0e00ab1e54783c7cef02080d
RecklessRyujin <recklesscollective1920@gmail.com>
2026-07-06 01:13:52 -0400

    Initialize ThemeManager in onCreate method

--- app/src/main/java/com/gamescout/MainActivity.java
+++ app/src/main/java/com/gamescout/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ThemeManager.init(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
